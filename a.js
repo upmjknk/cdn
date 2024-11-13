052b590fa36c5595119cc14c3de12629b5fb58e6
upmjknk <upmjknk@gmail.com>
2024-11-13 14:24:07 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 (function() {
 function printHelloWorld()
 {
-    console.log("Hello world from script hosted on github repo and deliverd through jsDelivr");
+    console.log("Hello world from script hosted on github repo and deliverd through jsDelivr nahhh");
 },
 })();
